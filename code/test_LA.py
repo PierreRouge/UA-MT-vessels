5834492d7cc4dd8a2a54e900b1ec7c4b39978779
Pierre Rougé <rouge@eq2-cf-626.creatis.insa-lyon.fr>
2023-09-01 09:57:59 +0200

    adapt tes for unet

--- code/test_LA.py
+++ code/test_LA.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import torch
-from networks.vnet import VNet
+from networks.unet import TinyUnet
 from test_util import test_all_case
 
 parser = argparse.ArgumentParser()
@@ -24,7 +24,10 @@ image_list = [FLAGS.root_path + '2018LA_Seg_Training Set/' + item.replace('\n', 
 
 
 def test_calculate_metric(epoch_num):
-    net = VNet(n_channels=1, n_classes=num_classes, normalization='batchnorm', has_dropout=False).cuda()
+    features = (32, 64, 128, 256)
+    kernel_size = (3, 3, 3, 3)
+    strides = (1, 2, 2, 2)
+    net = TinyUnet(dim=3, in_channel=1, features=features, strides=strides, kernel_size=kernel_size, nclasses=2) #change to U-Net for fair comparaison
     save_mode_path = os.path.join(snapshot_path, 'iter_' + str(epoch_num) + '.pth')
     net.load_state_dict(torch.load(save_mode_path))
     print("init weight from {}".format(save_mode_path))
