f5c51788ad5f3328dc10908bbe892a55d61ad295
Pierre Rougé <rouge@eq2-cf-626.creatis.insa-lyon.fr>
2023-12-15 18:23:45 +0100

    try debug

--- code/train_LA_meanteacher_certainty_unlabel.py
+++ code/train_LA_meanteacher_certainty_unlabel.py
@@ -111,13 +111,16 @@ if __name__ == "__main__":
     model = create_model()
     ema_model = create_model(ema=True)
 
+    # db_train = LAHeart(base_dir=train_data_path,
+    #                    split='train',
+    #                    transform = transforms.Compose([
+    #                       RandomRotFlip(),
+    #                       RandomCrop(patch_size),
+    #                       ToTensor(),
+    #                       ]))
     db_train = LAHeart(base_dir=train_data_path,
                        split='train',
-                       transform = transforms.Compose([
-                          RandomRotFlip(),
-                          RandomCrop(patch_size),
-                          ToTensor(),
-                          ]))
+                       transform = None)
     db_test = LAHeart(base_dir=train_data_path,
                        split='test',
                        transform = transforms.Compose([
